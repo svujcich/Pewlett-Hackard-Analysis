1c75dc5f5a9fe7f13557af07ad2a76b32f83d4d6
Sarah <sarah.vujcich.school@gmail.com>
2022-08-01 11:50:20 -0700

    Adding queries, retirement data, and minor adjustments to schema

--- schema.sql
+++ schema.sql
@@ -51,7 +51,7 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no, title)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
 SELECT * FROM departments;
