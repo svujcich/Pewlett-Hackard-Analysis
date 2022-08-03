c7aba828616d205f9deb2a6bc1a9b4fc211e2bbc
Sarah <sarah.vujcich.school@gmail.com>
2022-08-03 08:33:56 -0700

    adding a line of code that affects subsequent table creation

--- Employee_Database_challenge.sql/challenge_queries.sql
+++ Employee_Database_challenge.sql/challenge_queries.sql
@@ -12,6 +12,7 @@ FROM employees AS e
 	INNER JOIN titles AS t
 		ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (t.to_date = '9999-01-01')
 ORDER BY emp_no ASC;
 
 
@@ -52,3 +53,29 @@ FROM employees AS e
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 	 AND (de.to_date = '9999-01-01')
 ORDER BY e.emp_no ASC;
+
+
+
+-- Readme Overview details
+-- find the year Pwelett- Hackard was established
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.hire_date
+--INTO ph_start_date
+FROM employees AS e
+ORDER BY e.hire_date ASC;
+--established 1985
+
+--find total# of current employees
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.to_date
+INTO current_employees
+FROM employees AS e
+	INNER JOIN titles AS t
+		ON (e.emp_no = t.emp_no)
+WHERE (t.to_date = '9999-01-01')
+ORDER BY e.emp_no ASC;
+--240,124 current emp
